f4fc23880db5c541167370117c76038937a32d43
Maycon Willian <mayconwas2018@gmail.com>
2021-09-10 23:44:09 -0300

    Criação do projeto FortMetais

---
+++ src/main/java/com/fortmetais/pesagem/ControleDePesagemApplication.java
@@ -0,0 +1,13 @@
+package com.fortmetais.pesagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDePesagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDePesagemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fortmetais/pesagem/ControleDePesagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fortmetais.pesagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDePesagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
