5f5f49411f39e0cbd942abb412dcb13deb75eb12
Maycon Willian <mayconwas2018@gmail.com>
2021-09-20 12:26:45 -0300

    Criado template referente a dashboard

--- src/main/resources/static/javascripts/dashboard.grafico.js
+++ src/main/resources/static/javascripts/dashboard.grafico.js
@@ -1,96 +1,45 @@
-/* Gráfico de Ordem de Serviço */
-var SistemaOS = SistemaOS || {};
+/* Gráfico de Pesagem */
+var ctx = document.getElementById('graficoPesagem').getContext('2d');
+var graficoOS = new Chart(ctx, {
+	type: 'line',
+	data: {
+		labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho'],
+		datasets: [{
+			label: 'Total de Pesagens',
+			data: [12, 30, 3, 5, 2, 3],
+			backgroundColor: "#36b9cc",
+			pointBorderColor: "#36b9cc",
+			pointBackgroundColor: "#2d9faf",
+		}]
+	},
+	options: {
+		scales: {
+			y: {
+				beginAtZero: true
+			}
+		}
+	}
+});
 
-SistemaOS.GraficoOSPorMes = (function() {
-	
-	function GraficoOSPorMes() {
-		this.ctx = $('#graficoOS')[0].getContext('2d');
+/* Gráfico de  Pesagem por Produto*/
+var ctx = document.getElementById('graficoPorProduto').getContext('2d');
+var graficoOS = new Chart(ctx, {
+	type: 'bar',
+	data: {
+		labels: ['Produto 1', 'Produto 2', 'Produto 3', 'Produto 4', 'Produto 5', 'Produto 6'],
+		datasets: [{
+			label: 'Produtos',
+			data: [12, 30, 5, 3, 8, 12],
+			backgroundColor: "#5578e0",
+			pointBorderColor: "#5578e0",
+			pointBackgroundColor: "#5578e0",
+		}]
+	},
+	options: {
+		scales: {
+			y: {
+				beginAtZero: true
+			}
+		}
 	}
-	
-	GraficoOSPorMes.prototype.iniciar = function() {
-		$.ajax({
-			url: 'ordens/totalPorMes',
-			method: 'GET', 
-			success: onDadosRecebidos.bind(this)
-		});
-	}
-	
-	function onDadosRecebidos(movimentacaoMes) {
-		var meses = [];
-		var valores = [1,20];
-		movimentacaoMes.forEach(function(obj) {
-			meses.unshift(obj.mes);
-			valores.unshift(obj.total);
-		});	
-	
-		var graficoOSPorMes = new Chart(this.ctx, {
-		    type: 'bar',
-		    data: {
-		    	labels: meses,
-		    	datasets: [{
-		    		label: 'Quantidade de O.S no mês',
-		    		backgroundColor: "#4e73df",
-	                pointBorderColor: "#4e73df",
-	                pointBackgroundColor: "#fff",
-	                data: valores
-		    	}]
-		    },
-		});
-	}
-	
-	return GraficoOSPorMes;
-	
-}());
-
-/* Gráfico de Atendimentos */
-SistemaOS.GraficoSuportePorMes = (function() {
-	
-	function GraficoSuportePorMes() {
-		this.ctx = $('#graficoSuporte')[0].getContext('2d');
-	}
-	
-	GraficoSuportePorMes.prototype.iniciar = function() {
-		$.ajax({
-			url: 'suportes/totalPorMes',
-			method: 'GET', 
-			success: onDadosRecebidos.bind(this)
-		});
-	}
-	
-	function onDadosRecebidos(controleMes) {
-		var meses = [];
-		var valores = [1,800];
-		controleMes.forEach(function(obj) {
-			meses.unshift(obj.mes);
-			valores.unshift(obj.total);
-		});	
-	
-		var graficoSuportePorMes = new Chart(this.ctx, {
-		    type: 'line',
-		    data: {
-		    	labels: meses,
-		    	datasets: [{
-		    		label: 'Quantidade de Suporte no mês',
-		    		backgroundColor: "#36b9cc",
-	                pointBorderColor: "#4e73df",
-	                pointBackgroundColor: "#fff",
-	                data: valores
-		    	}]
-		    },
-		});
-	}
-	
-	return GraficoSuportePorMes;
-	
-}());
-
-$(function() {
-	
-	var graficoOSPorMes = new SistemaOS.GraficoOSPorMes();
-	graficoOSPorMes.iniciar();
-	
-	var graficoSuportePorMes = new SistemaOS.GraficoSuportePorMes();
-	graficoSuportePorMes.iniciar();
-	
-	
 });
